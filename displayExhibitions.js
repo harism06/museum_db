5b0c4f3f7ec1240f91520ae4ec19ea748f47f880
rain998 <131094474+rain998@users.noreply.github.com>
2024-11-16 12:12:11 -0600

    Add files via upload
    these need to be implemented

---
+++ displayExhibitions.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// MySQL connection
+const db = mysql.createConnection({
+  host: "museumdb.mysql.database.azure.com", 
+  database: "museumdb",                      
+  user: "user",                              
+  password: "password12345",                 
+});
+
+// Connect to MySQL
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+    process.exit(1); // Exit the application if there's a connection error
+  }
+  console.log('Connected to the database!');
+});
+
+// Fetch all artists (selecting all columns with SELECT *)
+app.get('/exhibition', (req, res) => {
+  db.query('SELECT * FROM exhibition', (err, results) => {
+    if (err) {
+      console.error('Error fetching Exhibitions:', err);
+      return res.status(500).send('Error retrieving Exhibitions');
+    }
+    res.json(results); // Send the artist data as JSON
+  });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
